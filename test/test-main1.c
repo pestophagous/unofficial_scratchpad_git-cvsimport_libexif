920fa56a8b1069db1d8f7fb50e36743a8421956a
pestophagous <pestophagous@users.noreply.github.com>
2016-02-21 20:06:20 -0800

    (again) populating even more data fields in this main, to make the analysis go further

--- test/test-main1.c
+++ test/test-main1.c
@@ -1,12 +1,11 @@
 
 #include "libexif/exif-entry.h"
 
-#include <stdio.h>
+#include <stdio.h>       // printf
+#include <string.h>      // memset
 
-void main(void)
+int main(void)
 {
-    printf("a\n");
-
     ExifData one_jpeg;
     one_jpeg.data = 0;
     one_jpeg.size = 0;
@@ -15,23 +14,67 @@ void main(void)
     // ExifContent->entries[n] holds items of type ExifEntry
     ExifContent an_idf;
     an_idf.entries = 0;
-    an_idf.count = 6;
+    an_idf.count = 64; // not yet used in this test (on feb 20). count of entries?
     an_idf.parent = &one_jpeg;
     an_idf.priv = 0;
 
-    char fakebuf[12];
+    unsigned char tag_data_buffer[400];  // add nondeterminism to this buffer.
+    unsigned char some_char = 0xFF;
+    memset(tag_data_buffer, some_char, 400);
+    some_char = 0x22;
+    /* tag_data_buffer[0] = 0xFF; */
+    tag_data_buffer[1] = some_char;
+    /* tag_data_buffer[2] = 0xFF; */
 
-    ExifEntry the_entry;
-    the_entry.tag = 1;        // ExifTag
-    the_entry.format = 2;     // ExifFormat
-    the_entry.components = 1; // unsigned long (1 for non-array types)
+    ExifEntry the_entries[3];
+    the_entries[0].tag = EXIF_TAG_ISO_SPEED_RATINGS;   // ExifTag. 0-0xa500(42240). try 0-65535
+    the_entries[0].format = EXIF_FORMAT_SHORT;     // ExifFormat. valid 1-12. try 0-20
+    the_entries[0].components = 1; // unsigned long (1 for non-array types). try 0-20.
 
-    the_entry.size = 12;
-    the_entry.data = fakebuf;
+    the_entries[0].size = 2;  // 1,2,4, or 8 (i think). try 0-20.
+    the_entries[0].data = tag_data_buffer;
 
-    the_entry.parent = &an_idf;
-    the_entry.priv = 0;
-    char valout[10];
+    the_entries[0].parent = &an_idf;
+    the_entries[0].priv = 0;
 
-    exif_entry_get_value(&the_entry,valout, 10);
+    the_entries[1].tag = EXIF_TAG_XP_AUTHOR;   // ExifTag. 0-0xa500(42240). try 0-65535
+    the_entries[1].format = EXIF_FORMAT_BYTE;     // ExifFormat. valid 1-12. try 0-20
+    the_entries[1].components = 1; // unsigned long (1 for non-array types). try 0-20.
+
+    the_entries[1].size = 1;  // 1,2,4, or 8 (i think). try 0-20.
+    the_entries[1].data = tag_data_buffer;
+
+    the_entries[1].parent = &an_idf;
+    the_entries[1].priv = 0;
+
+    the_entries[2].tag = EXIF_TAG_EXPOSURE_TIME;   // ExifTag. 0-0xa500(42240). try 0-65535
+    the_entries[2].format = EXIF_FORMAT_RATIONAL;     // ExifFormat. valid 1-12. try 0-20
+    the_entries[2].components = 1; // unsigned long (1 for non-array types). try 0-20.
+
+    the_entries[2].size = 8;  // 1,2,4, or 8 (i think). try 0-20.
+    the_entries[2].data = tag_data_buffer;
+
+    the_entries[2].parent = &an_idf;
+    the_entries[2].priv = 0;
+
+    an_idf.entries = the_entries;
+
+    one_jpeg.ifd[0] = &an_idf;
+    one_jpeg.ifd[1] = &an_idf;
+    one_jpeg.ifd[2] = &an_idf;
+    one_jpeg.ifd[3] = &an_idf;
+    one_jpeg.ifd[4] = &an_idf;
+
+    char valout[200];
+
+    exif_entry_get_value(&the_entries[0], valout, 200);
+    printf("output of exif_entry_get_value: %s\n", valout);
+
+    exif_entry_get_value(&the_entries[1], valout, 200);
+    printf("output of exif_entry_get_value: %s\n", valout);
+
+    exif_entry_get_value(&the_entries[2], valout, 200);
+    printf("output of exif_entry_get_value: %s\n", valout);
+
+    return 0;
 }
