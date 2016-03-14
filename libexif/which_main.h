54b8c0b0c58f7c0ba2b2bf30b32d1518c7e05c71
pestophagous <pestophagous@users.noreply.github.com>
2016-03-13 21:26:21 -0700

    (1 of N -- strncat test). this version of main_strncat_1 provokes zero (0) alarms from the analyzer

--- libexif/which_main.h
+++ libexif/which_main.h
@@ -2,7 +2,7 @@
 #ifndef FOR_ANALYSIS_DEF_MAIN_H
 #define FOR_ANALYSIS_DEF_MAIN_H
 
-#define main_strncat_2 main
+#define main_strncat_1 main
 
 
 #endif // FOR_ANALYSIS_DEF_MAIN_H
---
+++ test/test-main-scratch.c
@@ -0,0 +1,48 @@
+
+//#include "libexif/exif-entry.h"
+
+#include <stdio.h>       // printf
+#include <string.h>      // memset
+
+#ifdef __FRAMAC__
+#    include "__fc_string_axiomatic.h"
+#    include "__fc_builtin.h"
+#else
+
+unsigned char
+Frama_C_unsigned_char_interval(unsigned char min, unsigned char max);
+
+int
+Frama_C_interval(int min, int max);
+
+unsigned char Frama_C_unsigned_char_interval(unsigned char min, unsigned char max)
+{
+    return (rand() % 26) + 65;
+}
+
+int Frama_C_interval(int min, int max)
+{
+    return 1;
+}
+
+#endif // __FRAMAC__
+
+#include "which_main.h"
+
+int main_strncat_1(void)
+{
+    size_t ii = 0;
+
+    unsigned char test_buff[10];
+    for ( ii = 0; ii < 10; ii++ )
+        test_buff[ii] = Frama_C_unsigned_char_interval( 1, 0xFF );
+
+    //test_buff[10-2] = Frama_C_unsigned_char_interval( 0,1 );
+    test_buff[10-1] = 0;
+
+    // for strncat: the size of dest must be at least strlen(dest)+n+1.
+
+    strncat (test_buff, " ", (9) - strlen(test_buff));
+
+    return 0;
+}
